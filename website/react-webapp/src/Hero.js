eb9a8e9d8ccbefaa25adfdf82329b407285c8089
Abhishek AN <darkabhi1520@gmail.com>
2020-10-24 12:08:20 +0530

    Log out confirmation dialog and display Email in header
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- website/react-webapp/src/Hero.js
+++ website/react-webapp/src/Hero.js
@@ -1,14 +1,57 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import fire from "./fire";
 
-const Hero = ({ handleLogout }) => {
+export default function AlertDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleLogout = () => {
+    fire.auth().signOut();
+  };
+
+  const user = fire.auth().currentUser;
+  user.providerData.forEach((userInfo) => {
+    console.log("User info for provider: ", userInfo);
+  });
+
   return (
-    <section className="hero">
-      <nav>
-        <h2>Welcome</h2>
-        <button onClick={handleLogout}>Logout</button>
-      </nav>
-    </section>
-  );
-};
+    <div>
+      <section className="hero">
+        <nav>
+          <h2>Welcome {user.email}!</h2>
+          <Button onClick={handleClickOpen} disableElevation>
+            Logout
+          </Button>
+        </nav>
+      </section>
 
-export default Hero;
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Are you sure?"}</DialogTitle>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleLogout} color="primary" autoFocus>
+            Logout
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
