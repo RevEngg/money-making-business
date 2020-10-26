ff80bda7aff0bedc9932ee6c05ebe80f9ffc5dfe
Abhishek AN <darkabhi1520@gmail.com>
2020-10-26 12:42:25 +0530

    Add firestore query add query Modification required after dropdowns added
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- website/src/Hero.js
+++ website/src/Hero.js
@@ -4,37 +4,46 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import firebase from "./fire";
-import storage from "./fire"
 
 export default function Dashboard() {
   const [open, setOpen] = React.useState(false);
   const [progress, setProgress] = useState(0);
 
   const onChange = (e) => {
-      const file = e.target.files[0];
-      const uploadTask = firebase.storage().ref().child(file.name).put(file);
-      uploadTask.on(
-        "state_changed",
-        snapshot => {
-          const progress = Math.round(
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          );
-          setProgress(progress);
-        },
-        error => {
-          console.log(error);
-        },
-        () => {
-            firebase.storage()
-            .ref()
-            .child(file.name)
-            .getDownloadURL()
-            .then(url => {
-              console.log(url)
-            });
-        }
-      );
-  }
+    const file = e.target.files[0];
+    const uploadTask = firebase.storage().ref().child(file.name).put(file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progress);
+      },
+      (error) => {},
+      () => {
+        firebase
+          .storage()
+          .ref()
+          .child(file.name)
+          .getDownloadURL()
+          .then((url) => {
+            firebase
+              .firestore()
+              .collection("files")
+              .doc()
+              .set({
+                downloadUrl: url,
+                fileName: e.target.files[0].name,
+                fileSize: e.target.files[0].size,
+                fileType: e.target.files[0].type,
+                uploadTime: firebase.firestore.FieldValue.serverTimestamp(),
+              })
+              .catch((err) => {});
+          });
+      }
+    );
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -49,8 +58,7 @@ export default function Dashboard() {
   };
 
   const user = firebase.auth().currentUser;
-  user.providerData.forEach((userInfo) => {
-  });
+  user.providerData.forEach((userInfo) => {});
 
   return (
     <div>
@@ -61,11 +69,9 @@ export default function Dashboard() {
             Logout
           </Button>
         </nav>
-        <input type="file" onChange={onChange}/>
+        <input type="file" onChange={onChange} />
         <progress value={progress} max="100" />
       </section>
-
-      
 
       <Dialog
         open={open}
